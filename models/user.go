8c189f418c5e16dc61031ef258e4e8e43c0957c7
Daniel Didah <didahdaniel@gmail.com>
2020-04-24 08:36:57 +0300

    added user function for update,delete,get user

--- models/user.go
+++ models/user.go
@@ -1,26 +1,65 @@
 package models
 
-type User struct{
-	ID int
-	FirstName string
+import (
+	"errors"
+	"fmt"
+)
+
+type User struct {
+	ID         int
+	FirstName  string
 	SecondName string
 }
 
-var(
-	users []*User
-	nextID =1
+var (
+	users  []*User
+	nextID = 1
 )
 
-
-func GetUsers() []*User{
+func GetUsers() []*User {
 	return users
 }
 
-func AddUser(u User)(User,error){
-  u.ID=nextID
-  nextID++
-  users=append(users,&u)
-  
-  return u,nil
-  
+func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("User must not include id or it must be set to zero")
+	}
+
+	u.ID = nextID
+	nextID++
+	users = append(users, &u)
+
+	return u, nil
+
+}
+
+func GetUserById(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User with id `%v` not found", id)
+}
+
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User with id `%v` not found", u.ID)
+}
+
+func RemoveUserById(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with id `%v` not found", id)
 }
